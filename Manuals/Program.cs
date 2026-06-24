b9c5b4a532a29e2e52bce1af787c103ca631d489
Chris Golden <4693287+crgolden@users.noreply.github.com>
2026-06-24 16:40:55 -0600

    Updating Service name

--- Manuals/Program.cs
+++ Manuals/Program.cs
@@ -81,6 +81,12 @@ try
                     {
                         elasticsearchSinkOptions.DataStream = new DataStreamName("logs", "dotnet", nameof(Manuals));
                         elasticsearchSinkOptions.BootstrapMethod = BootstrapMethod.Failure;
+                        elasticsearchSinkOptions.TextFormatting.MapCustom = (ecsDocument, _) =>
+                        {
+                            ecsDocument.Service ??= new Elastic.CommonSchema.Service();
+                            ecsDocument.Service.Name = applicationName;
+                            return ecsDocument;
+                        };
                     },
                     transportConfiguration =>
                     {
